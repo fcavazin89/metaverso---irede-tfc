839c98b39a0399d3ad91cf8a8ce44bf6981c27a4
fcavazin89 <profcavazinf@gmail.com>
2026-05-05 10:20:39 -0400

    Create VRSceneSetup.cs

---
+++ Assets/VRSceneSetup.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class VRSceneSetup : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("[VR] Cena carregada. Modo de teste PC ativo.");
+        Debug.Log("[VR] Lembre-se de configurar o XR Plugin Management antes do build Android.");
+    }
+}
